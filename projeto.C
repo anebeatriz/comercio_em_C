0178080f9847087215e1947b7e2aa072602d3d4b
Bia <159267437+anebeatriz@users.noreply.github.com>
2025-02-27 09:37:55 -0300

    Update projeto.C

--- projeto.C
+++ projeto.C
@@ -3,23 +3,40 @@
 
 int main() {
    char escolha;
+   char sair;
+   char estoque;
    printf("Digite E para estoque, V para venda, F para contole financeiro: ");
    scanf("%c" , &escolha);
+   
    switch(escolha) {
        case 'E':
        printf("Controle e estoque: \n");
-       printf("Produtos, quantidade, preços, validades, código do produto");
+       printf("____________________________________________________________________________________________ \n");
+       printf("Itens disponíveis: \n \n");
+       printf("Produto A");
        break;
        case 'V':
-       printf("Controle de venda: \n");
-       printf("Código do produto, preço");
+       printf("Controle de venda: ");
+       printf("Código do produto, preço \n");
        break;
        case 'F':
-       printf("Controle financeiro: \n");
-       printf("Descontos, lucros, depesas");
+       printf("Controle financeiro: ");
+       printf("Descontos, lucros, depesas \n");
+       break;
        default:
-       printf("Escolheu opção inválida");
+       printf("Escolheu opção inválida \n");
        break;
    }
+  /* printf("Você deseja sair do programa? \n");
+   printf("Para sair digite s, para voltar digite v \n");
+   scanf("%c" , &sair);
+   switch(sair) {
+       case 's':
+       printf("Programa encerrado \n");
+       break;
+       case 'v':
+       printf("Digite E para estoque, V para venda, F para contole financeiro: \n");
+   }*/
+   
     return 0;
 }
